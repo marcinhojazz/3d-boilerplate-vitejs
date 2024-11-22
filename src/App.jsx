f3abbe03678e125219be4b011f1b3f3aa0008aa9
marcinhojazz <marcinhojazz@gmail.com>
2024-11-22 11:48:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
+
+// Componente Box
+function Box(props) {
+  const meshRef = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      rotation={active ? [0, 0, 180] : [0, 0, 0]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Canvas>
+        <ambientLight intensity={2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} intensity={1} />
+        <pointLight position={[-10, -10, -10]} intensity={1} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </>
+  )
+}
+
+export default App
